00e62a68cdd7e9d84b7ccf2bd400233b1e854ccc
going2631 <tomo.moedora1212@gmail.com>
2021-07-13 12:33:49 +0000

    [Add]puma設定追記

---
+++ spec/factories/customers.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+
+  factory :customer do
+    first_name            {"neko"}
+    last_name             {"neko"}
+    first_name_kana       {"neko"}
+    last_name_kana        {"neko"}
+    telephone_number      {"neko"}
+    email                 {"neko@gmail.com"}
+    password              {"nyannyan"}
+    password_confirmation {"nyannyan"}
+  end
+
+end
