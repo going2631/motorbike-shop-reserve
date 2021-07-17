c1ffb388843ff6b8c87e024987d73da2b4575970
going2631 <tomo.moedora1212@gmail.com>
2021-07-17 09:43:56 +0000

    本デプロイ前最終

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -5,7 +5,7 @@ module LoginModule
       visit new_customer_session_path
       fill_in 'customer_email', with: customer.email
       fill_in 'customer_password', with: 'nyannyan'
-      click_button 'Log in'
+      click_button 'ログイン'
   end
 
 end
